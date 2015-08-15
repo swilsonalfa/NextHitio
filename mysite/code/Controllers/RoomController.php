5e1ccac3d7d1d948bcb231624b0c0330be2c2ba7
Stewart Wilson <stewartwilson@outlook.com>
2015-08-15 20:38:35 +1000

    create room from homepage then redirect

--- mysite/code/Controllers/RoomController.php
+++ mysite/code/Controllers/RoomController.php
@@ -1,10 +1,72 @@
 <?php
 class RoomController extends MainController {
 	private static $allowed_actions = array(
-			'index'
+			'index',
+			'createajax'
 	);
 	
 	public function index($request) {
 		return $this->customise(array("Content" => $this->renderWith("room_index")));
+	}
+	
+	public function createajax($request) {
+		// Only accept if it's an ajax request
+		if($this->request->isAjax()) {
+			// all we need is the event title to create a room. other options can be set later
+			$roomname = Convert::raw2sql($_POST['eventname']);
+			$errors = array();
+			
+			if(empty($roomname)) {
+				$errors[] = "You must have an event name";
+			}
+			
+			$roomNameSegment = $this->generateSegment($roomname);
+			
+			if(empty($roomNameSegment) && !empty($roomname)) {
+				$errors[] = "Room name is not valid.";
+			}
+			
+			// check to ensure something doesn't already exist
+			$rooms = Room::get_one("Room", "Segment = '$roomNameSegment'");
+			
+			if($rooms && !empty($roomname)) {
+				$errors[] = "A Room with that name exists. Try again.";
+			}
+			
+			if($errors) {
+				$returnArray = array();
+			
+				$returnArray["success"] = false;
+				$returnArray["errorstring"] = "<div class=\"alert alert-info\">".implode(", ", $errors)."</div>";
+			
+				return json_encode($returnArray);
+			} else {
+				// No errors. Let's make the room
+				$room = new Room();
+				$room->Segment = $roomNameSegment;
+				$room->Title = $roomname;
+				$roomID = $room->write();
+				
+				if($roomID) {
+					// All good.
+					Session::set("RoomAdmin", $roomID);
+					
+					$returnArray = array();
+						
+					$returnArray["success"] = true;
+					$returnArray["nextstep"] = "/room/".$roomNameSegment;
+						
+					return json_encode($returnArray);
+					
+				}
+			}
+		}
+	}
+	
+	public function generateSegment($name) {
+		$filter = URLSegmentFilter::create();
+		$t = $filter->filter($name);
+		
+		return $t;
 	}
 }
--- themes/nexthit/templates/Layout/index.ss
+++ themes/nexthit/templates/Layout/index.ss
@@ -1,6 +1,14 @@
 <div class="jumbotron">
   <div class="container">
     <h2>Start your NextHit</h2>
-    <h3>Music, Pizza, Judging</h3>
+    <h3>People powered music. Vote for your favourite music.</h3>
+    <div id="roomformresponse"></div>
+    <form class="form-inline ajaxform" method="post" name="roomform" action="/room/createajax">
+	  <div class="form-group">
+	    <label for="eventname">My event is called</label>
+	    <input type="text" class="form-control" id="eventname" name="eventname" placeholder="Sarah's Birthday">
+	  </div>
+	  <button type="submit" class="btn btn-default">Go</button>
+	</form>
   </div>
 </div>
